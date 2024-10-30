7e27e83a6c568a5ec7cf5001a4841c20fb5ec67a
fomin2601 <fomin260145@gmail.com>
2024-10-30 21:23:48 +0300

    - fix problem with parameter engine which was required in endpoints

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 from typing import Annotated
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from sqlmodel import Session
 
-from .models.utils import create_db_engine, create_db_and_tables, get_session
+from .models.utils import create_db_engine, create_db_and_tables, get_session, engine
 from .routers import rooms, users
 
-engine = create_db_engine()
-
 app = FastAPI()
+
 app.include_router(rooms.router)
 #app.include_router(users.router)
 
 @app.on_event('startup')
 def on_startup():
     create_db_and_tables(engine)
+
+
 
--- app/models/utils.py
+++ app/models/utils.py
@@ -2,6 +2,8 @@ from typing import Annotated
 from sqlmodel import create_engine, SQLModel, Session
 from fastapi import Depends
 
+
+
 def create_db_engine():
     sqlite_file_name = "database.db"
     sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -11,11 +13,13 @@ def create_db_engine():
 
     return engine
 
+engine = create_db_engine()
+
 def create_db_and_tables(engine):
     SQLModel.metadata.create_all(engine)
 
 
-def get_session(engine):
+def get_session():
     with Session(engine) as session:
         yield session
 
