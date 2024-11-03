df27b3df5d5ca12019c53e7122d15a70bccbf29d
fomin2601 <fomin260145@gmail.com>
2024-11-03 08:01:50 +0300

    - Add exception handling for room creating

--- app/controllers/rooms.py
+++ app/controllers/rooms.py
@@ -1,4 +1,5 @@
 from sqlmodel import select
+from sqlalchemy.exc import IntegrityError
 from app.models.utils import SessionDep
 from app.models.rooms import Room
 
@@ -9,7 +10,16 @@ def get_room_info(session: SessionDep, room_id: int):
 
 
 def create_room(session: SessionDep, room: Room):
-    session.add(room)
-    session.commit()
-    session.refresh(room)
+    try:
+        session.add(room)
+        session.commit()
+        session.refresh(room)
+
+    except IntegrityError:
+        room.id = None
+        session.rollback()
+        session.add(room)
+        session.commit()
+        session.refresh(room)
+
     return room
