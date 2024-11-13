d5eab8cfc0e40e3bd701df2d7850f312c9e85170
fomin2601 <fomin260145@gmail.com>
2024-11-13 22:40:11 +0300

    - Fix CORS support

--- app/main.py
+++ app/main.py
@@ -2,21 +2,18 @@ from fastapi import FastAPI
 
 from app.internal.utils import create_db_and_tables, engine
 from .routers import rooms, users, auth
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
 
-app = FastAPI()
+middleware = [Middleware(CORSMiddleware, allow_origins=True)]
+
+app = FastAPI(middleware=middleware)
 
 app.include_router(rooms.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 
 @app.on_event('startup')
 def on_startup():
