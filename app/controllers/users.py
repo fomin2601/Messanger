ff3bd83bbd608dab1189ec5b24e3ae2a9f8f0cbc
fomin2601 <fomin260145@gmail.com>
2024-11-17 22:13:13 +0300

    - Add getting user's rooms

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -2,10 +2,25 @@ from sqlalchemy import select
 
 from app.internal.utils import SessionDep
 from app.models.users import UserDB
+from app.models.links import RoomUserLink
+from app.models.rooms import Room
 
 
 def get_all_users(session: SessionDep):
     statement = select(UserDB)
-    users = [user[0] for user in session.exec(statement).all() if user is not None]
+    users = session.exec(statement).scalars().all()
 
     return users
+
+
+def get_rooms_of_user(session: SessionDep, user_id: int):
+    statement = select(RoomUserLink).where(RoomUserLink.user_id == user_id)
+    room_ids = (room.room_id for room in session.exec(statement).scalars().all())
+
+    if not room_ids:
+        return False
+
+    statement = select(Room).where(Room.id.in_(room_ids))
+    rooms = session.exec(statement).scalars().all()
+
+    return rooms
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends, status, Response, HTTPException
-from fastapi.encoders import jsonable_encoder
+from fastapi import APIRouter, Depends, status, HTTPException
 from typing import List
 
 from app.internal.utils import JWTBearer, SessionDep
 from app.controllers import users
 from app.models.users import User
+from app.models.rooms import Room
 
 router = APIRouter(
     prefix='/users',
@@ -16,3 +16,16 @@ router = APIRouter(
 @router.get('/userlist', status_code=status.HTTP_200_OK, response_model=List[User])
 async def get_all_users(session: SessionDep):
     return users.get_all_users(session)
+
+
+@router.get('/{user_id}', status_code=status.HTTP_200_OK, response_model=List[Room])
+async def get_rooms_of_user(session: SessionDep, user_id: int):
+    rooms = users.get_rooms_of_user(session=session, user_id=user_id)
+
+    if not rooms:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User is not included in any room'
+        )
+
+    return rooms
