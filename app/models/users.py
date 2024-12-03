6a053bcfed9fdbe3b6da14ddda19d8a2d9ef2c24
fomin2601 <fomin260145@gmail.com>
2024-12-03 21:03:57 +0300

    - Use class from typing instead of builtin list

--- app/models/users.py
+++ app/models/users.py
@@ -34,7 +34,7 @@ class UserDB(User, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True)
     hashed_password: str = Field()
-    rooms: list["Room"] = Relationship(
+    rooms: List["Room"] = Relationship(
         back_populates='users',
         link_model=RoomUserLink
     )
