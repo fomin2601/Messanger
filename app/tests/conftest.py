f84b91aa54d7c6b143335b6ddd8330938c546aa0
fomin2601 <fomin260145@gmail.com>
2025-01-24 01:26:15 +0300

    - Refactor creating fixture with DB, add predefined data in DB

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,92 +1,71 @@
 import pytest
-import os
-from sqlmodel import create_engine, Session
+from sqlmodel import create_engine, Session, insert
 from sqlalchemy.engine import Engine
 from sqlmodel import SQLModel
-from typing import Dict, List, Union
-
-from app.internal.utils import create_db_and_tables
-from app.models.users import UserDB
-from app.models.links import UserRoleLink
+from sqlmodel.pool import StaticPool
+from app.schemes.users import UserRegistrationScheme
 
 
-@pytest.fixture(scope='session')
-def db_url() -> str:
-    db_password = os.environ.get('MESSENGER_DB_PASSWORD')
-    db_url = f"postgresql://postgres:{db_password}@localhost:5432/test_database_messanger"
+predefined_tables = {
+    'userrole': [
+        {'id': 0, 'role_name': 'Администратор'},
+        {'id': 1, 'role_name': 'Руководитель'},
+        {'id': 2, 'role_name': 'Методист'},
+        {'id': 3, 'role_name': 'Владелец'},
+    ],
+    'userdb': [
+        {
+            'id': 0,
+            'username': 'default_username',
+            'hashed_password': 'default_hashed_password',
+            'is_active': True,
+            'first_name': 'default_name',
+            'second_name': 'default_second_name',
+            'patronymic': 'default_patronymic',
+            'description': 'default_description',
+        }
+    ],
+    'userrolelink': [
+        {'user_id': 0, 'role_id': 0},
+        {'user_id': 0, 'role_id': 1},
+        {'user_id': 0, 'role_id': 2},
+        {'user_id': 0, 'role_id': 3},
+    ]
+}
 
-    return db_url
 
-
-@pytest.fixture(scope='session')
-def db_engine(db_url: str) -> Engine:
-    """Yields a SQLAlchemy engine which will be suppressed after the test session"""
-
-    engine_ = create_engine(db_url, echo=True)
-
-    yield engine_
-
-    engine_.dispose()
+def insert_default_db_data(engine: Engine):
+    with engine.connect() as connection:
+        for table in predefined_tables:
+            connection.execute(
+                insert(SQLModel.metadata.tables[table]),
+                predefined_tables[table]
+            )
+            connection.commit()
 
 
 @pytest.fixture(scope='class')
-def db_tables(db_engine: Engine):
-    """Initialize database's tables"""
-    create_db_and_tables(db_engine)
+def db_session() -> Session:
+    db_engine = create_engine(
+        "sqlite://",
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
+    SQLModel.metadata.create_all(db_engine)
+    insert_default_db_data(db_engine)
 
-    yield
-
-    SQLModel.metadata.drop_all(bind=db_engine)
-
-
-@pytest.fixture(scope='class')
-def db_session(db_engine: Engine) -> Session:
-    # connection = db_engine.connect()
-    # transaction = connection.begin()
-    # session = Session(bind=connection)
-    #
-    # yield session
-    #
-    # session.close()
-    # transaction.rollback()
-    # connection.close()
     with Session(db_engine) as db_session:
         yield db_session
 
 
 @pytest.fixture(scope='function')
-def active_user_parameters() -> Dict[str, Union[int, str, bool]]:
-    return {
-        'id': 1,
-        'username': 'test_username',
-        'hashed_password': 'test_hashed_password',
-        'is_active': True,
-        'first_name': 'test_name',
-        'second_name': 'test_second_name',
-        'patronymic': 'test_patronymic',
-        'description': 'test_description'
-    }
-
-
-@pytest.fixture(scope='function')
-def all_roles_for_active_user(active_user_parameters: Dict[str, Union[int, str, bool]]) -> List[Dict[str, int]]:
-    return [
-        {'user_id': 1, 'role_id': 0},
-        {'user_id': 1, 'role_id': 1},
-        {'user_id': 1, 'role_id': 2},
-        {'user_id': 1, 'role_id': 3}
-    ]
-
-
-@pytest.fixture(scope='module')
-def active_user_in_db(
-        db_session: Session,
-        user_parameters: Dict[str, Union[int, str, bool]],
-        roles_for_user: List[Dict[str, int]]
-):
-    user_entity = UserDB.parse_obj(user_parameters)
-    db_session.add(user_entity)
-    db_session.commit()
-
-    db_session.bulk_insert_mappings(UserRoleLink, roles_for_user)
-    db_session.commit()
+def user_entity() -> UserRegistrationScheme:
+    return UserRegistrationScheme(
+            username='test_username',
+            hashed_password='test_hashed_password',
+            first_name='test_name',
+            second_name='test_surname',
+            patronymic='test_patronymic',
+            roles=[0, 1, 2, 3],
+            description='test_description',
+        )
